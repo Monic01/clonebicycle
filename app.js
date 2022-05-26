f0eb0243ac568c00bee110cd948b0b0e16056b8c
Monic01 <eudemonic01@gmail.com>
2022-05-26 14:54:01 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.querySelector("#chatbot").addEventListener("click", function () {
+  document.querySelector(".chat-box").classList.toggle("hidden");
+  document.querySelector(".bot-img").classList.toggle("active");
+});
